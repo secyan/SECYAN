3bc98b12f5cd8559e16d130bb0a59e6978105af3
YANG Nan <nyangakll@gmail.com>
2021-08-04 00:55:48 +0800

    add max(annotation) function

--- src/python_wrapper/relation_wrapper.h
+++ src/python_wrapper/relation_wrapper.h
@@ -14,6 +14,7 @@
 namespace py = pybind11;
 
 void init_relationship(py::module &m);
+void init_AggregateType(py::module &m);
 void init_DataType(py::module &m);
 void init_e_role(py::module &m);
 void init_relation_info(py::module &m);
